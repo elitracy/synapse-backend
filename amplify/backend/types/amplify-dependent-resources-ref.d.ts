a81e0c140e494ac844c98a66f11ca4dc9e2be85d
Elias Tracy <elitracy19@gmail.com>
2023-02-20 10:34:34 -0600

    express with graphql and amplify added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "synapsebackend": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import express from "express"
-const app = express();
-const port = 8080; // default port to listen
+const app = express()
 
-// define a route handler for the default home page
+import { Amplify } from 'aws-amplify';
+import { DataStore } from '@aws-amplify/datastore';
+import { User } from './models/index.js';
+import config from "../src/aws-exports.js";
+
+Amplify.configure({
+  ...config,
+});
+
 app.get("/", (_, res) => {
-  res.send("Hello world!");
-});
+  res.send("Hello World!")
+})
 
-// start the Express server
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
-});
+app.get("/users", async (_, res) => {
+  const models = await DataStore.query(User);
+  res.send(models);
+})
+
+app.post("/users", async (req, res) => {
+  const { name, email } = req.query
+
+  await DataStore.save(
+    new User({
+      "Name": name,
+      "Email": email
+    })
+  ).then(() => {
+    res.send("User added!")
+  }).catch((err) => {
+    res.send(err)
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
+
