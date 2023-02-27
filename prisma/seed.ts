9b344dddc61d37069e16704cab211ded9b434136
Elias Tracy <elitracy19@gmail.com>
2023-02-27 12:16:47 -0600

    fixed merge. removed join from schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,46 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+
+async function main() {
+
+
+  await prisma.group.create({
+    data: {
+      Name: "Test Group",
+      Users: {
+        create: [{
+          Name: 'Alice',
+          Email: 'alice@example.com',
+          Password: 'alicepass'
+        },
+        {
+          Name: 'Bob',
+          Email: 'bob@example.com',
+          Password: 'bobpass'
+        },
+        {
+          Name: 'Charlie',
+          Email: 'charlie@example.com',
+          Password: 'charliepass'
+        },
+        {
+          Name: 'Professor',
+          Email: 'professor@example.com',
+          Password: 'professorpass',
+        }
+        ]
+      }
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
