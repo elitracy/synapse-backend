128b7b14c6a6bdba8a04a97ce024de7df93a6c4d
EricWendel <eawendel19@gmail.com>
2023-03-01 10:18:04 -0600

    Basic API endpoints for notes

---
+++ api/notes.ts
@@ -0,0 +1,69 @@
+import express from "express"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const router = express.Router()
+
+router.get('/', async (_, res) => {
+  prisma.note.findMany().then((note) => {
+    res.status(200).json(note)
+  }).catch((err) => {
+    res.status(400).json(err)
+  })
+})
+
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+
+  prisma.note
+    .findUnique({
+      where: { id },
+    }).then((note) => {
+      res.status(200).json(note)
+    }).catch((err) => {
+      res.status(400).json(err)
+    })
+})
+
+
+
+router.post('/', async (req, res) => {
+  const { context, userId } = req.body
+  prisma.note.create({
+    data: {
+      context, userId
+    },
+  }).then((note) => {
+    res.status(200).json(note)
+  }).catch((err) => {
+    res.status(400).json(err)
+  })
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  prisma.note.delete({
+    where: { id },
+  }).then((note) => {
+    res.status(200).json(note)
+  }).catch((err) => {
+    res.status(400).json(err)
+  })
+})
+
+router.put('/context/:id', async (req, res) => {
+  const { id } = req.params
+  let { context } = req.body
+
+  prisma.note.update({
+    where: { id },
+    data: { context },
+  }).then((note) => {
+    res.status(200).json(note)
+  }).catch((err) => {
+    res.status(400).json(err)
+  })
+})
+
+export default router
--- index.ts
+++ index.ts
@@ -7,11 +7,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 import users from "./api/users"
+import notes from "./api/notes"
 
 app.use(express.json())
 app.use(express.static('public'))
 
 app.use("/api/users", users)
+app.use("/api/notes", notes)
 
 app.get('/', (_, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, 'public') });
