87104fc2fd3e5a66f25719e6eeafb730406966f9
Elias Tracy <elitracy19@gmail.com>
2023-03-22 14:19:50 -0500

    removed utils folder from older iteration

--- utils/generateOutline.ts
+++
@@ -1,6 +0,0 @@
-export default async function(transcript: string): Promise<string> {
-  setTimeout(() => {
-    console.log('[LOG] Generating outline')
-  }, 10000)
-  return 'I am a test outline'
-}
--- utils/generateTranscript.ts
+++
@@ -1,6 +0,0 @@
-export default async function(media: any): Promise<string> {
-  setTimeout(() => {
-    console.log('[LOG] Generating transcript')
-  }, 10000)
-  return 'I am a test transcript'
-}
