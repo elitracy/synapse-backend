c87495c0614c51c6e8d2491685b84c2a6ce534a5
EricWendel <eawendel19@gmail.com>
2023-04-01 19:49:00 -0500

    Test to create, query, delete user

--- deployed.test.js
+++ deployed.test.js
@@ -4,7 +4,7 @@ const frontend = 'https://www.synapsenote.com';
 const backend = 'https://api.synapsenote.com';
 
 describe('Frontend Status', () => {
-	it('should return a 200 status code', async () => {
+	it('Home page 200 status code', async () => {
 		const response = await request(frontend)
 			.get('/');
 
@@ -13,26 +13,40 @@ describe('Frontend Status', () => {
 });
 
 describe('Backend Status', () => {
-	it('should return a 200 status code', async () => {
+	it('Api home page 200 status code', async () => {
 		const response = await request(backend)
 			.get('/');
 
 		expect(response.statusCode).toBe(200);
 	});
+});
 
-
+let userID;
+describe('Backend User Operations', () => {
 	it('Make a user', async () => {
 		const response = await request(backend)
-		  .post('/api/users')
-		  .send({
+			.post('/api/users')
+			.send({
 			"name": "jest",
 			"email": "jest@gmail.com",
 			"password": "jestPass"
-		  });
+			});
 		expect(response.statusCode).toBe(200)
-		// expect(response.body).toMatchObject({completed: true, id: 1})
-	  });
+		userID = response._body.id;
+	});
+	
+	it('Query a user by ID', async () => {
+		const response = await request(backend)
+			.get(`/api/users/${userID}`);
+		expect(response._body.name).toBe('jest')
+		expect(response._body.email).toBe('jest@gmail.com')
+		expect(response.statusCode).toBe(200)
+	});
+
+	it('Delete a user by ID', async () => {
+		const response = await request(backend)
+			.delete(`/api/users/${userID}`);
+		expect(response.statusCode).toBe(200)
+	});
+
 });
-
-
-
