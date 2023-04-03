6a5374346570b0384d7cae58abb311857b6b6bdf
EricWendel <eawendel19@gmail.com>
2023-04-03 10:08:50 -0500

    Test note operations

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -54,3 +54,28 @@ describe('Backend User Operations', () => {
 	});
 
 });
+
+
+let noteID;
+describe('Backend Note Operations', () => {
+	it('Make a note', async () => {
+		const response = await request(app)
+			.post('/api/notes');
+		expect(response.statusCode).toBe(200)
+		noteID = response._body.id;
+	});
+	
+	it('Query a note by ID', async () => {
+		const response = await request(app)
+			.get(`/api/notes/${noteID}`);
+		expect(response.statusCode).toBe(200)
+	});
+
+	it('Delete a note by ID', async () => {
+		const response = await request(app)
+			.delete(`/api/notes/${noteID}`);
+		expect(response.statusCode).toBe(200)
+	});
+
+});
+
